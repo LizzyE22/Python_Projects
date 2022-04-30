7698bf03fd5a2dfb673755e0f860a52cbbc3b29a
LizzyE22 <esqueda22@gmail.com>
2022-04-29 19:55:58 -0700

    push

---
+++ inheritance.py
@@ -0,0 +1,16 @@
+
+#parent class 
+class User:
+    name = 'Sandy'
+    email = ''
+    account_number = 9
+
+#child class Employee with unique attributes 
+class Employee(User):
+    position = 'manager'
+    weekly_hours = 40
+
+#child class Guest with unique attributes
+class Client(User):
+    phone_number = ''
+    wine_club = True
