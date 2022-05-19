99861734305d431c06cfd378a18c8ebe93b3eb8d
LizzyE22 <esqueda22@gmail.com>
2022-05-19 12:16:24 -0700

    push

--- Python_challenges.py/file_transfer_part2.py
+++ Python_challenges.py/file_transfer_part2.py
@@ -3,11 +3,21 @@ import shutil
 import os
 
 #set where the source of the files are
-source = '/Users/Esque/OneDrive/Documents/GitHub/Python_Projects/Python_challenges.py/hold_file/'
+source = '/Users/Esque/OneDrive/Documents/GitHub/Python_Projects/Python_challenges.py/hold_file/hold_text.txt'
 
-#set the destination path to folderB
-destination = '/Users/Esque/OneDrive/Documents/GitHub/Python_Projects/Python_challenges.py/receive_file/'
+#set the destination path 
+destination = '/Users/Esque/OneDrive/Documents/GitHub/Python_Projects/Python_challenges.py/receive_file'
 files = os.listdir(source)
 
 #copying content of source file to destination file
-shutil.copyfile('source', 'destination')
+shutil.copyfile(source, destination)
+
+for file in files:
+    #gets the absolute path of the file, which includes the source folder path and the file name
+    absolutePath = (source + 'hold_text.txt')
+    #moves the file located at the absolute path to the destination folder
+    shutil.copyfile(absolutePath, destination)
+
+
+
+
