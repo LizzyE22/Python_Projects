65f715262c56753645b2f5c69fad801a98dba507
LizzyE22 <esqueda22@gmail.com>
2022-05-19 14:42:10 -0700

    push

--- file_transfer_part2.py
+++ file_transfer_part2.py
@@ -3,14 +3,14 @@ import shutil
 import os
 
 #set where the source of the files are
-source = '/Users/Esque/OneDrive/Documents/GitHub/Python_Projects/hold_file/'
+source = '/Users/Esque/OneDrive/Documents/GitHub/Python_Projects/Python_challenges.py/hold_file/'
 
 #set the destination path 
-destination = '/Users/Esque/OneDrive/Documents/GitHub/Python_Projects/receive_file/'
+destination = '/Users/Esque/OneDrive/Documents/GitHub/Python_Projects/Python_challenges.py/'
 files = os.listdir(source)
 
 #copying content of source file to destination file
-#shutil.copyfile(source, destination)
+shutil.copyfile(source, destination)
 
 for file in files:
     #gets the absolute path of the file, which includes the source folder path and the file name
